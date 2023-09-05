dddec974bc6f64bbb1a2b9dcbdfc27c911a1c193
Rao Imtinan <2019cs32@stu.mnsuet.edu.pk>
2023-09-05 10:46:56 -0700

    variable and datatypes  standard investigation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID=4278
+let accountEmail = "hec@gmail.com"
+var accountPassword= "Banglore"
+city = "khanewal"
+let accountSummary;
+console.table([accountID,accountEmail,accountPassword,city,accountSummary])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"; //use  for teat allJS code as newer version
+
+// alert("Hello") we are using node is not possible
+
